46dc8e558a5e1fc79d6ffd8c06e71c3934eb6e0e
Aaron Sherber <aaron@sherber.com>
2023-06-24 10:39:25 -0400

    Use adjacent assign if availale

--- src/measure_hide_empty.lua
+++ src/measure_hide_empty.lua
@@ -2,6 +2,7 @@ function plugindef()
     finaleplugin.RequireScore = true
     finaleplugin.RequireSelection = true
     finaleplugin.RequireDocument = true
+    finaleplugin.MinJWLuaVersion = 0.65
     finaleplugin.Author = "Aaron Sherber"
     finaleplugin.AuthorURL = "https://aaron.sherber.com"
     finaleplugin.Copyright = "CC0 https://creativecommons.org/publicdomain/zero/1.0/"
@@ -20,7 +21,8 @@ function plugindef()
         "Applies the \"Hide Staff\" staff style to empty measures."
 end
 
-local function get_hide_staff_id()
+
+local function get_hide_staff_style_id()
     for s in loadall(finale.FCStaffStyleDefs()) do
         local str = finale.FCString()
         s:GetName(str)
@@ -30,27 +32,63 @@ local function get_hide_staff_id()
     end
 end
 
-local function cell_has_staff_style(m, s, style_id)
-    for a in loadall(finale.FCStaffStyleAssigns()) do
+local function get_staff_style_assign(m, s, style_id)
+    local assigns = finale.FCStaffStyleAssigns()
+    assigns:LoadAllForItem(s)
+    for a in each(assigns) do
         if a.StartMeasure <= m and a.EndMeasure >= m
-                and a.Staff == s and a.StyleID == style_id then
-            return true
+                and a.StyleID == style_id then
+            return a
         end
     end
-    return false
 end
 
 local function hide_empty_measures()
-    local hide_staff_id = get_hide_staff_id()
-    local region = finenv.Region()
-    for m, s in eachcell(region) do
+    local hide_staff_style_id = get_hide_staff_style_id()
+
+    local function get_hide_staff_style_assign(m, s)
+        return get_staff_style_assign(m, s, hide_staff_style_id)
+    end
+
+    local function is_candidate_cell(m, s)
         local cell = finale.FCCell(m, s)
-        if not cell:CalcContainsEntries() and not cell_has_staff_style(m, s, hide_staff_id) then
-            local assign = finale.FCStaffStyleAssign()
-            assign.StyleID = hide_staff_id
-            assign.StartMeasure = m
-            assign.EndMeasure = m
-            assign:SaveNew(s)
+        return not cell:CalcContainsEntries()
+            and not get_hide_staff_style_assign(m, s)
+    end
+
+
+    local document = finale.FCMusicRegion()
+    document:SetFullDocument()
+    local doc_measure_count = document:CalcMeasureSpan()
+
+    local region = finenv.Region()
+    for range_start, staff_id in eachcell(region) do
+        if is_candidate_cell(range_start, staff_id) then
+            local range_end = range_start
+
+            while region:IsMeasureIncluded(range_end + 1) and is_candidate_cell(range_end + 1, staff_id) do
+                range_end = range_end + 1
+            end
+
+            local previous_assign = range_start > 1
+                and get_hide_staff_style_assign(range_start - 1, staff_id)
+            local next_assign = range_end < doc_measure_count
+                and get_hide_staff_style_assign(range_end + 1, staff_id)
+
+            if previous_assign then
+                previous_assign.EndMeasure = range_end
+                previous_assign:Save()
+            elseif next_assign then
+                next_assign.StartMeasure = range_start
+                next_assign:Save()
+            else
+                local assign = finale.FCStaffStyleAssign()
+                assign.StyleID = hide_staff_style_id
+                assign.StartMeasure = range_start
+                assign.EndMeasure = range_end
+                assign:SaveNew(staff_id)
+            end
+
         end
     end
 end
--- src/playground.lua
+++ src/playground.lua
@@ -2,13 +2,14 @@ function plugindef()
     return "Lua Playground", "", "Lua Playground"
 end
 
---[[
+---[[
 local home = os.getenv("HOME") or os.getenv("HOMEDRIVE") .. os.getenv("HOMEPATH")
 package.path = home .. "/.vscode/extensions/alexeymelnichuk.lua-mobdebug-0.0.5/lua/?.lua"
     .. ";" .. package.path
 local mobdebug = require ("vscode-mobdebug")
 mobdebug.start('127.0.0.1', 8172)
 --]]
+
 
 
 dofile(finenv.RunningLuaFolderPath() .. "measure_hide_empty.lua")
