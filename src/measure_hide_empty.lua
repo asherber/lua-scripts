5afbea027e0aa5cf7b005d191213d212e7120589
Aaron Sherber <aaron@sherber.com>
2023-06-24 12:30:38 -0400

    Choose from multiple staff styles

--- src/measure_hide_empty.lua
+++ src/measure_hide_empty.lua
@@ -6,30 +6,71 @@ function plugindef()
     finaleplugin.Author = "Aaron Sherber"
     finaleplugin.AuthorURL = "https://aaron.sherber.com"
     finaleplugin.Copyright = "CC0 https://creativecommons.org/publicdomain/zero/1.0/"
-    finaleplugin.Version = "0.9.1"
-    finaleplugin.Date = "2023-06-21"
+    finaleplugin.Version = "0.9.2"
+    finaleplugin.Date = "2023-06-24"
     finaleplugin.Id = "4aebe066-d648-4111-b8b3-22ac2420c37d"
     finaleplugin.RevisionNotes = [[
         v0.9.1      First internal version
+        v0.9.2      Reuse adjacent staff style assignments
+                    Pick from multiple "Hide Staff" staff styles
     ]]
     finaleplugin.Notes = [[
-        This script will apply the "Hide Staff" staff style to any measures in
-        the selected region that do not have any entries.
+        This script will apply a "Hide Staff" staff style to any measures in
+        the selected region that do not have any entries. If you have more
+        than one "Hide Staff" staff style defined, you can pick the one
+        you want to use.
     ]]
 
     return "Hide Empty Measures", "Hide Empty Measures",
-        "Applies the \"Hide Staff\" staff style to empty measures."
+        "Applies a \"Hide Staff\" staff style to empty measures."
 end
 
 
+local function pick_style(styles)
+    if #styles == 0 then
+        finenv.UI():AlertInfo('No "Hide Staff" staff style found.', "Error")
+        return
+    elseif #styles == 1 then
+        return next(styles)
+    end
+
+    local function make_str(str)
+        local s = finale.FCString()
+        s.LuaString = str
+        return s
+    end
+
+    local dialog = finale.FCCustomWindow()
+    dialog:SetTitle(make_str('Select "Hide Staff" Style'))
+
+    local group = dialog:CreateRadioButtonGroup(0, 0, #styles)
+    local labels = finale.FCStrings()
+    local max_width = 0
+    for _, style in ipairs(styles) do
+        labels:AddCopy(make_str(style[2]))
+        max_width = math.max(max_width, style[2]:len() * 6)
+    end
+    group:SetText(labels)
+    group:SetWidth(max_width)
+
+    dialog:CreateOkButton()
+    dialog:CreateCancelButton()
+    if dialog:ExecuteModal(nil) == finale.EXECMODAL_OK then
+        return styles[group:GetSelectedItem() + 1]
+    end
+end
+
 local function get_hide_staff_style_id()
-    for s in loadall(finale.FCStaffStyleDefs()) do
+    local hide_staff_styles = {}
+    for def in loadall(finale.FCStaffStyleDefs()) do
         local str = finale.FCString()
-        s:GetName(str)
+        def:GetName(str)
         if str.LuaString:find("Hide Staff") then
-            return s.ItemNo
+            table.insert(hide_staff_styles, { def.ItemNo, str.LuaString })
         end
     end
+
+    return pick_style(hide_staff_styles)
 end
 
 local function get_staff_style_assign(m, s, style_id)
@@ -45,6 +86,9 @@ end
 
 local function hide_empty_measures()
     local hide_staff_style_id = get_hide_staff_style_id()
+    if not hide_staff_style_id then
+        return
+    end
 
     local function get_hide_staff_style_assign(m, s)
         return get_staff_style_assign(m, s, hide_staff_style_id)
